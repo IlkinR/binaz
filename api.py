71d0a98575b6580da6450ca2bb1fefb7f0204430
Ilkin Ramazanov <babalke44@gmail.com>
2021-06-18 09:30:26 +0400

    updated product url template

--- api.py
+++ api.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 
 from services import collect_product_data
 
-PRODUCT_URL_TEMPLATE = 'https://bina.az/items/{product_id}'
+PRODUCT_URL_TEMPLATE = 'https://bina.az/items/{id}'
 
 app = FastAPI()
 
 
 @app.get("/product/{product_id}")
 async def root(product_id: int):
-    product_url = PRODUCT_URL_TEMPLATE.format(product_id=product_id)
+    product_url = PRODUCT_URL_TEMPLATE.format(id=product_id)
     product = collect_product_data(product_url)
     return {"url": product_url, 'product': product}
